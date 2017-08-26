324417b46ecbfacf0006f67181f3795f65f8ae5e
daniel <dvago1988@gmail.com>
2017-08-26 05:10:44 -0500

    Listo el metodo para agregar produtos, se empieza la implementacion del flashmessage

--- src/main/java/com/danielvargas/InventarioWeb/controller/ProductosController.java
+++ src/main/java/com/danielvargas/InventarioWeb/controller/ProductosController.java
@@ -5,9 +5,14 @@ import com.danielvargas.InventarioWeb.service.ProductosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.TreeSet;
 
 /**
  * Created by Daniel on 20/08/2017
@@ -32,7 +37,17 @@ public class ProductosController {
     }
 
     @RequestMapping("/agregar")
-    public String agregar(Model model) {
+    public String formularioAgregar(Model model) {
+
+        if (!model.containsAttribute("productos")) {
+            //agregamos el modelo para poder usar el formulario
+            model.addAttribute("productos", new Productos());
+        }
+
+        model.addAttribute("accion","/agregar");
+        model.addAttribute("encabezado","Agregar");
+        model.addAttribute("boton","Agregar");
+
         return "formulario";
     }
 
@@ -40,4 +55,33 @@ public class ProductosController {
     public String eliminar(Model model) {
         return "formulario";
     }
+
+    @RequestMapping(value = "/agregar", method = RequestMethod.POST)
+    public String agregarProducto(@Valid Productos productos, BindingResult result, RedirectAttributes redirectAttributes){
+        if (result.hasErrors()) {
+            //Esto es para optener los erroes de validacion cuando redirijamos
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.category", result);
+
+            //Agregamos categoria si data invalida fue recibida
+            //Esto lo tendremos disponible solo por una redireccionada, despues se perderá
+            redirectAttributes.addFlashAttribute("category", productos);
+
+            //redirigimos hacia donde estaba antes
+            return "redirect:/categories/add";
+        }
+
+        productosService.agregarProducto(productos);
+
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage("Producto exitosamente agregado", FlashMessage.Status.SUCCESS));
+
+        /*
+        El redirect hace que spring mande un request del tipo 302 (de redireccionamiento)
+        ademas hay que tener cuidado pues despues de los 2 puntos, NO se pone el template sino la pagina a la que
+        me quiero redireccionar.
+         */
+        return "redirect:/agregar";
+
+    }
 }
+
+
