eb2f8371ee66568de1f45d5e7f430c39264eebfa
daniel <dvago1988@gmail.com>
2017-11-30 21:07:47 -0500

    base de datos local postgresql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
 -- INSERT INTO role (name) VALUES ('ROLE_USER');
 -- password = pass   https://www.browserling.com/tools/bcrypt
--- INSERT INTO user (username, enabled, password, role_id) VALUES ('daniel', TRUE, '$2a$10$tFOphGnmxAbfq.okCOLYJu1dEPtii4z/bnEgd7K0GAkCfPPepZ6n6', 1);
+-- password = salami   https://www.browserling.com/tools/bcrypt
+-- password = carola   https://www.browserling.com/tools/bcrypt
+-- INSERT INTO "user" (id, username, enabled, password, role_id) VALUES (1, 'daniel', TRUE, '$2a$10$tFOphGnmxAbfq.okCOLYJu1dEPtii4z/bnEgd7K0GAkCfPPepZ6n6', 1);
+-- INSERT INTO "user" (id, username, enabled, password, role_id) VALUES (2, 'marcos', TRUE, '$2a$10$2L6lJrv/gEWHetxC4/TfretRj9fge3adGNIT22/bjRm9SoDURyYf2', 1);
+-- INSERT INTO "user" (id, username, enabled, password, role_id) VALUES (3, 'jesus', TRUE, '$2a$10$n61NF16JCr9UEl0YoHARter8g9/2Ahr27vJK8ynyZ/ZKiUrqmK/k2', 1);
 -- INSERT INTO proveedor (nombre, descripcion, telefono) VALUES ('baks', 'ddsdiso', 123456);
 -- INSERT INTO productos (nombre, precio, precioEntrada, cantidad, descripcion,cantidadVendido, proveedor_id) VALUES ('prod1', 1200, 1000, 12, 'bla bla', 1, 1);
